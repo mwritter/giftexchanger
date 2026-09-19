f171ddabdd57b0376f13d98c4ee40085dcb2d88b
Matthew Ritter <matthewwilliamritter@gmail.com>
2026-09-19 14:06:03 -0400

    feat: connect the api to the frontend

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+const API_URL = `${process.env.API_URL ?? "http://localhost:8080"}`
+
 const nextConfig: NextConfig = {
-  /* config options here */
+
+async rewrites() {
+  return [
+    {
+      source: "/api/:path*",
+      destination: `${API_URL}/:path*`,
+    },
+  ];
+}
 };
 
 export default nextConfig;
